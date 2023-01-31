b930d557005b364ea03d34d5db2f43edae51e6f9
Saif <119940317+MohammedSaifullah9@users.noreply.github.com>
2023-01-31 15:56:12 +0530

    Add files via upload

---
+++ TestJavascript.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+function add() {
+
+    var numOne = 10;
+    var numTwo = 20;
+    var sum = numOne + numTwo;
+    document.write("Sum = " + sum);
+}
+
+function multiply(p1, p2) {
+    return p1 * p2;
+}
